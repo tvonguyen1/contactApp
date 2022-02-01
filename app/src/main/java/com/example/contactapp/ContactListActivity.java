57748af9a0c30ee5bdbac543f1c6796e966ee9b7
tvonguyen1 <nhattienvo21@gmail.com>
2022-02-01 12:58:49 -0500

    Database configuration

--- app/src/main/java/com/example/contactapp/ContactListActivity.java
+++ app/src/main/java/com/example/contactapp/ContactListActivity.java
@@ -2,7 +2,10 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ContactListActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class ContactListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        initListButton();
+        initMapButton();
+        initSettingButton();
     }
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.contactList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactListActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.map);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactListActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void initSettingButton() {
+        ImageButton ibList = findViewById(R.id.setting);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactListActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/contactapp/ContactMapActivity.java
+++ app/src/main/java/com/example/contactapp/ContactMapActivity.java
@@ -2,7 +2,10 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ContactMapActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class ContactMapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_map);
+        initListButton();
+        initMapButton();
+        initSettingButton();
     }
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.contactList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactMapActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.map);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactMapActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void initSettingButton() {
+        ImageButton ibList = findViewById(R.id.setting);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ContactMapActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
